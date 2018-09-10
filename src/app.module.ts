0677630825b03c47a0362be7d326eaae37508c86
Christian Schröter <dev@cschroeter.net>
2018-09-10 16:10:32 +0200

    chore: Update to latest deps

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,57 +1,30 @@
-import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import { GraphQLModule, GraphQLFactory } from '@nestjs/graphql';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppResolvers } from './app.resolver';
-import { SubscriptionsService } from './subscriptions/subscriptions.service';
-import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 import { Prisma } from './generated/prisma';
 
 @Module({
-  imports: [GraphQLModule, SubscriptionsModule.forRoot()],
-  controllers: [AppController],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      path: '/',
+      installSubscriptionHandlers: true,
+      resolverValidationOptions: {
+        requireResolversForResolveType: false,
+      },
+    }),
+  ],
   providers: [
-    AppService,
     AppResolvers,
     {
       provide: Prisma,
       useFactory: () => {
         return new Prisma({
-          endpoint: '<YOUR ENDPOINT>',
+          endpoint: 'https://eu1.prisma.sh/public-agatepuma-476/my-app/dev',
           debug: false,
         });
       },
     },
   ],
 })
-export class ApplicationModule implements NestModule {
-  constructor(
-    private readonly graphQLFactory: GraphQLFactory,
-    private readonly subscriptionsService: SubscriptionsService,
-  ) {}
-
-  configure(consumer: MiddlewareConsumer) {
-    const typeDefs = this.graphQLFactory.mergeTypesByPaths('src/**/*.graphql');
-    const schema = this.graphQLFactory.createSchema({ typeDefs });
-
-    this.subscriptionsService.createSubscriptionServer(schema);
-
-    consumer
-      .apply(
-        graphiqlExpress({
-          endpointURL: '/graphql',
-          subscriptionsEndpoint: `ws://localhost:5001/subscriptions`,
-        }),
-      )
-      .forRoutes('/graphiql')
-      .apply(
-        graphqlExpress(req => ({
-          schema,
-          rootValue: req,
-          context: req,
-        })),
-      )
-      .forRoutes('/graphql');
-  }
-}
+export class ApplicationModule {}
